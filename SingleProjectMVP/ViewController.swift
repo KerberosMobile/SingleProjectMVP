356132e9114a308f0dc2a9718271debc0beea0be
Nikita <nikita@nikitas-mac.local>
2016-09-04 06:23:56 -0700

    Initial Commit

---
+++ SingleProjectMVP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingleProjectMVP
+//
+//  Created by Nikita on 9/4/16.
+//  Copyright © 2016 ksenia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
